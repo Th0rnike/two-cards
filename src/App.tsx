46b097a12df296b72c51405c29c1253c8484725b
Bald Eagle <tsereteli.t02@gmail.com>
2023-12-26 18:39:13 +0400

    added elements briefly and starting to write funcitonality

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,49 @@ import cardLogo from "./assets/card-logo.svg";
 
 function App() {
   return (
-    <div className="cards-container">
-      <div className="back-card">
-        <img src={backCard} alt="" />
-        <span>000</span>
-      </div>
-      <div className="front-card">
-        <img src={frontCard} alt="" />
-        <div className="card-details">
-          <img id="logo" src={cardLogo} alt="" />
-          <span>0000 0000 0000 0000</span>
-          <div className="info">
-            <span>jane appleseed</span>
-            <span>00/00</span>
+    <>
+      <div className="cards-container">
+        <div className="back-card">
+          <img src={backCard} alt="back of the card" />
+          <span>000</span>
+        </div>
+        <div className="front-card">
+          <img src={frontCard} alt="front of the card" />
+          <div className="card-details">
+            <img id="logo" src={cardLogo} alt="logo of the card" />
+            <span>0000 0000 0000 0000</span>
+            <div className="info">
+              <span>jane appleseed</span>
+              <span>00/00</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="input-div">
+        <form>
+          <div>
+            <label htmlFor="holder">Cardholder Name</label>
+            <input id="holder" type="text" />
+          </div>
+          <div>
+            <label htmlFor="cardNumber">Card Number</label>
+            <input id="cardNumber" type="text" />
+          </div>
+          <div className="expiration-dates">
+            <div>
+              <label htmlFor="mm">exp. date (mm/yy)</label>
+              <input id="mm" type="number" />
+              <input id="yy" type="number" />
+            </div>
+            <div>
+              <label htmlFor="cvc">cvc</label>
+              <input id="cvc" type="number" />
+            </div>
+          </div>
+        </form>
+        <button className="confirm">confirm</button>
+      </div>
+    </>
   );
 }
 
